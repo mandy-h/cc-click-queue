d6c9672c70f55fdbbe921f24266ed1289a9f7505
Mandy H <flyingpanduh@gmail.com>
2023-01-02 21:56:31 -0500

    Functionality to add multiple adopts via context menu

--- scripts/extension/quick-add.js
+++ scripts/extension/quick-add.js
@@ -1,3 +1,7 @@
+/**
+ * Functionality to add adopts to the queue via context menu.
+ * @param {Object} info - info object passed from chrome.contextMenus.onClicked
+ */
 function quickAdd(info) {
   // Copied from ExtensionStorage.js
   const ExtensionStorage = {
@@ -29,11 +33,6 @@ function quickAdd(info) {
 
   // Function to add adopt to queue, copied from Queue.js
   async function add(adoptableIds, targetLevel) {
-    // Null check in case the user doesn't enter a target level in the prompt
-    if (!targetLevel) {
-      return;
-    }
-
     const result = await ExtensionStorage.get({ queue: [] });
     const { queue } = result;
 
@@ -62,9 +61,57 @@ function quickAdd(info) {
     ExtensionStorage.set({ queue });
   }
 
-  const targetLevel = window.prompt('Enter target level:');
-  const adoptableId = info.srcUrl.includes('/images/adoptables/') && info.srcUrl.match(/\d+/);
-  if (typeof adoptableId?.[0] === 'string') {
-    add(adoptableId, targetLevel);
+  // Copied from ImageDrop.js
+  function parseIdsAndImagesFromHtml(htmlString) {
+    const parser = new DOMParser();
+    const imageUrls = Array.from(parser.parseFromString(htmlString, 'text/html')
+      .body.querySelectorAll('img[src*="/images/adoptables/"]'))
+      .map((image) => image.src);
+    const adoptableIds = imageUrls.map((url) => url.match(/\d+/)?.[0]);
+
+    return { adoptableIds, imageUrls };
   }
+
+  /**
+   * Returns HTML of a highlighted selection.
+   * [Source]{@link https://stackoverflow.com/a/5084044}
+   */
+  function getHtmlOfSelection() {
+    let range;
+    if (document.selection && document.selection.createRange) {
+      range = document.selection.createRange();
+      return range.htmlText;
+    } else if (window.getSelection) {
+      const selection = window.getSelection();
+      if (selection.rangeCount > 0) {
+        range = selection.getRangeAt(0);
+        const clonedSelection = range.cloneContents();
+        const div = document.createElement('div');
+        div.appendChild(clonedSelection);
+        return div.innerHTML;
+      } else {
+        return '';
+      }
+    } else {
+      return '';
+    }
+  }
+
+  (function init() {
+    const targetLevel = window.prompt('Enter target level:');
+    const selection = getHtmlOfSelection();
+
+    if (selection) {
+      // User right-clicked on a highlighted selection
+      const selection = getHtmlOfSelection();
+      const { adoptableIds } = parseIdsAndImagesFromHtml(selection);
+      add(adoptableIds, targetLevel);
+    } else {
+      // User right-clicked on an image
+      const adoptableId = info.srcUrl.includes('/images/adoptables/') && info.srcUrl.match(/\d+/);
+      if (typeof adoptableId?.[0] === 'string' && targetLevel) {
+        add(adoptableId, targetLevel);
+      }
+    }
+  }());
 }
