0cb49ec9018238043d810f6c315d87b13f06e49f
mandy-h <16712197+mandy-h@users.noreply.github.com>
2025-02-25 20:06:56 -0500

    Split Queue.js business logic and view logic

--- scripts/extension/app.js
+++ scripts/extension/app.js
@@ -1,17 +1,8 @@
-import ExtensionStorage from '/scripts/modules/ExtensionStorage.js';
-import { init as initQueue } from '/scripts/modules/Queue.js';
+import { init as initQueue } from '/scripts/modules/Queue/Queue.js';
 import { init as initImageDrop } from '/scripts/modules/ImageDrop.js';
 
 (async function () {
   // Initialize queue page
-  const result = await ExtensionStorage.get({ queue: [] });
-  window.App = {
-    state: {
-      queue: result.queue,
-      queueLength: result.queue.length
-    }
-  };
-
   initQueue();
   initImageDrop();
 })();
