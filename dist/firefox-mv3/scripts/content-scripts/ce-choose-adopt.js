adf3e332ce3e80ad22c9cca6cf814c351a2bfe64
mandy-h <16712197+mandy-h@users.noreply.github.com>
2026-06-05 19:18:37 -0400

    Delete dist folder and add it to gitignore

--- dist/firefox-mv3/scripts/content-scripts/ce-choose-adopt.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-  if (window.location.hash === '#done') {
-    const toast = Message.createToast('Queue finished', 'success');
-    const mainContent = document.querySelector('#megaContent center');
-    mainContent.insertBefore(toast, mainContent.firstChild);
-  }
-});
