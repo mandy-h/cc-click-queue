26029574989847bfe7edcc88ce202a03fa4f0606
mandy-h <16712197+mandy-h@users.noreply.github.com>
2026-06-08 23:18:54 -0400

    Automatically switch to first adopt in queue if the CE home page doesn't show it as active

--- src/scripts/content-scripts/ce-do/ce-do.js
+++ src/scripts/content-scripts/ce-do/ce-do.js
@@ -2,7 +2,10 @@
   const extensionStorageScript = chrome.runtime.getURL('/scripts/modules/ExtensionStorage.js');
   const coreModuleScript = chrome.runtime.getURL('/scripts/content-scripts/ce-do/ce-do-core.js');
 
-  const [{ default: ExtensionStorage }, { getAdoptableLevel, handleQueueUpdate }] = await Promise.all([
+  const [
+    { default: ExtensionStorage },
+    { getAdoptableLevel, handleQueueUpdate, displayToast, switchToNextAdoptable }
+  ] = await Promise.all([
     import(extensionStorageScript),
     import(coreModuleScript)
   ]);
@@ -13,7 +16,21 @@
   }
 
   const currentLevel = getAdoptableLevel();
-  if (currentLevel > -1) {
+  const params = new URLSearchParams(window.location.search);
+  const isMainCePage = params.size === 0;
+  const isQueueAdoptActive = document.querySelector(`[src*="/adoptables/${queue[0].id}.gif"]`);
+
+  if (isMainCePage && !isQueueAdoptActive) {
+    displayToast('Loading...', 'info');
+
+    const successfullyLoaded = await switchToNextAdoptable(queue[0].id);
+
+    if (successfullyLoaded) {
+      displayToast('Queue is active, but the first adoptable was not found. Switched to first adoptable in queue.', 'info');
+    } else {
+      displayToast('Failed to switch adoptable', 'error');
+    }
+  } else if (currentLevel > -1) {
     await handleQueueUpdate(currentLevel);
   }
 }());
